05042a7202d60010fc82d8b87a805225874b3570
lmeadows <meadows.lucas@gmail.com>
2021-10-08 15:26:30 -0600

    putting canvas element into html and wiring up initial rust-wasm -> js bridge

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod utils;
 
+use js_sys::WebAssembly;
 use wasm_bindgen::prelude::*;
+use wasm_bindgen::JsCast;
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -8,12 +10,28 @@ use wasm_bindgen::prelude::*;
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-#[wasm_bindgen]
-extern {
-    fn alert(s: &str);
-}
+use gloo::{
+    events::EventListener,
+    render::{request_animation_frame, AnimationFrame},
+};
+use web_sys::{
+    AudioBuffer, AudioContext, Event, HtmlCanvasElement, KeyboardEvent, WebGlBuffer, WebGlProgram,
+    WebGlRenderingContext, WebGlShader, WebGlTexture, WebGlUniformLocation,
+};
 
-#[wasm_bindgen]
-pub fn greet() {
-    alert("Hello, pinball!");
+#[wasm_bindgen(start)]
+pub fn start() -> Result<(), JsValue> {
+    utils::set_panic_hook();
+
+    let window = web_sys::window().unwrap();
+    let document = window.document().unwrap();
+    let canvas: HtmlCanvasElement = document.get_element_by_id("canvas").unwrap().dyn_into()?;
+    let ctx_options = js_sys::Object::new();
+    js_sys::Reflect::set(&ctx_options, &"alpha".into(), &false.into()).unwrap();
+    let gl: WebGlRenderingContext = canvas
+        .get_context_with_context_options("webgl", &ctx_options)?
+        .unwrap()
+        .dyn_into()?;
+
+    Ok(())
 }
--- www/index.js
+++ www/index.js
@@ -1,3 +1,3 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "pinball";
 
-wasm.greet();
+//wasm.greet();
